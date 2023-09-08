cf4a940af42ae0f6c66b92f59277c304a81de501
Kristina Dudjak <dudjakkristina@gmail.com>
2023-09-08 11:54:34 +0200

    fix: animation

--- slider.js
+++ slider.js
@@ -19,22 +19,42 @@ $(document).ready(function () {
 
   $('#btn_right').on('click', function () {
     let upperImage = $('.slider__up img:last-child')
+    let list = $('.slider__up')
+    updateList(list, upperImage, 0.5)
+
     let lowerImage = $('.slider__down__list img:last-child')
-    upperImage.slideUp(500, function () {
-      $('.slider__up').prepend(upperImage.fadeIn(400))
-    })
-    let imagesWidth =
-      lowerImage.offset().left + upperImage.width() + lowerImage.width()
-    let maxWidth = $('.slider__down__arrows').offset().left
-    if (imagesWidth < maxWidth) {
-      $('.slider__down__list').css(
-        'margin-left',
-        '+=' + upperImage.width() + 'px'
-      )
-    } else {
-      lowerImage.slideUp(500, function () {
-        $('.slider__down__list').prepend(lowerImage.fadeIn(400))
+    let lowerList = $('.slider__down__list')
+    updateList(lowerList, lowerImage, 0.8)
+
+    function updateList (list, image, duration) {
+      let leftMargin = parseFloat(list.css('marginLeft')) || 0
+      let currentWidth = image.offset().left + image.width()
+      let maxWidth = $('.slider__down__arrows-left').offset().left
+      let newMargin = 0
+
+      if (maxWidth < currentWidth) {
+        newMargin = maxWidth - currentWidth + 10 - leftMargin
+      } else {
+        newMargin = currentWidth - maxWidth + 10 - leftMargin
+      }
+
+      list.css('transition', `margin-left ${duration}s ease-out`)
+      list.css('marginLeft', -newMargin + 'px')
+
+      image.fadeTo(500, 0.5, function () {
+        image.prependTo(list)
+        image.css('opacity', 1)
+        list.css('marginLeft', leftMargin)
       })
+      setTimeout(function () {
+        image.prependTo(list)
+        if (newMargin > 120) {
+          list.css('marginLeft', leftMargin)
+        } else {
+          list.css('marginLeft', -newMargin - 20)
+        }
+        list.css('transition', 'none')
+      }, 500)
     }
   })
 })
